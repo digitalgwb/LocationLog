a256aeec337b5904403f710121317b6808a28480
Gary W. Black <digitalgwb@gmail.com>
2013-03-24 22:06:14 -0400

    Added details link to tracks table - fixed date defects

---
+++ LocationLog/PointTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  PointTableViewController.h
+//  LocationLog
+//
+//  Created by Gary Black on 3/24/13.
+//  Copyright (c) 2013 Parkmeadow Productions. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PointTableViewController : UITableViewController
+
+@property NSInteger trackKey;
+
+@end
---
+++ LocationLog/PointTableViewController.m
@@ -0,0 +1,15 @@
+//
+//  PointTableViewController.m
+//  LocationLog
+//
+//  Created by Gary Black on 3/24/13.
+//  Copyright (c) 2013 Parkmeadow Productions. All rights reserved.
+//
+
+#import "PointTableViewController.h"
+
+@implementation PointTableViewController
+
+@synthesize trackKey;
+
+@end
